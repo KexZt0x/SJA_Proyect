f206b2125b9edf68c4f0460b656760c8a9c9c096
KexZt0x <KexZ1t0x@gmail.com>
2024-11-21 21:58:11 -0500

    Backend

---
+++ Proyecto 11/routers/reportesRouter.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const conexion = require('../conexion');
+
+router.get('/masvendido', (req, res) => {
+    const query = `SELECT NombreProducto, SUM(CantidadProducto) AS TotalVendido
+    FROM Movimiento
+    JOIN Producto_Materia_Prima ON Movimiento.IdProductoMateriaPrima = Producto_Materia_Prima.IdProductoMateriaPrima
+    WHERE TipoMovimiento = 'Salida'
+    GROUP BY NombreProducto
+    ORDER BY TotalVendido DESC
+    LIMIT 3;`;
+    conexion.query(query, (error, resultado) => {
+        if (error) return console.error(error.message);
+
+        if (resultado.length > 0) {
+            res.json(resultado);
+        } else {
+            res.json(`No hay registros en la tabla de reportes`)
+        }
+    });
+});
+
+router.get('/menosvendido', (req, res) => {
+    const queryr = `SELECT NombreProducto, SUM(CantidadProducto) AS TotalVendido
+    FROM Movimiento
+    JOIN Producto_Materia_Prima ON Movimiento.IdProductoMateriaPrima = Producto_Materia_Prima.IdProductoMateriaPrima
+    WHERE TipoMovimiento = 'Salida'
+    GROUP BY NombreProducto
+    ORDER BY TotalVendido ASC
+    LIMIT 3;`;
+    conexion.query(queryr, (error, resultado) => {
+        if (error) return console.error(error.message);
+
+        if (resultado.length > 0) {
+            res.json(resultado);
+        } else {
+            res.json(`No hay registros en la tabla de reportes`)
+        }
+    });
+});
+
+module.exports = router; 
